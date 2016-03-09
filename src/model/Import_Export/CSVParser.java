829a04a78216e502e54f39078f38f37e00901b79
Eric Mai <ericmai231@gmail.com>
2016-03-09 18:27:20 -0500

    parse csv file into map

--- src/model/Import_Export/CSVParser.java
+++ src/model/Import_Export/CSVParser.java
@@ -1,52 +1,36 @@
-package Import_Export
+package Import_Export;
 
-import java.io.File;
-import java.util.Scanner;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import model.holding.Equity;
 
-/*
-Parses the CSV file and stors them as an ArrayList to be retreved latter
- */
 public class CSVParser {
-    private File file = File("../Resources/equities.csv");
-    private static ArrayList<Equity> stocks = new ArrayList<>();
 
-    /*
-    Parces the CSV File
-     */
-    public boolean parceStocks() {
-        try {
-            Scanner scanner = new Scanner(file);
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                stocks.add(parseLine(line));
+    public static HashMap<String, Double> tickerprice;
+
+    public static void main(String[] args) throws IOException {
+        tickerprice = new HashMap<>();
+        BufferedReader reader = new BufferedReader(new FileReader("../Resources/equities.csv"));
+        String line;
+
+        while((line = reader.readLine()) != null){
+            double price;
+            String ticker = line.split(",")[0];
+            Pattern pattern = Pattern.compile("\\d+.\\d+");
+            Matcher matcher = pattern.matcher(line);
+            if (matcher.find()){
+                try {
+                    price = Double.parseDouble(matcher.group(0));
+                } catch (NumberFormatException e) {
+                    continue;
+                }
+                tickerprice.put(ticker, price);
             }
-            scanner.close();
-        } catch (Exceptio ex) {
-            ex.printStackTrace();
-            return false;
         }
-
-        return true;
-
+        //System.out.println(tickerprice);
     }
-
-    /*
-    Returns the static ArrayList
-     */
-    public ArrayList<Equity> getStocks(){
-        return stocks;
-    }
-
-    /*
-    Processes a line.
-     */
-    private Equity parseLine(String line){
-        String ticker = line.split(',')[0];
-        double price = line.match("\d.\d");
-        return Equity equity = new Equity(ticker,-1,price);
-    }
-
 }
