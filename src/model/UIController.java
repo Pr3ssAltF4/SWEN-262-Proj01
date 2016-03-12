446ac42cad002394f2532521d3aa69a9c3672ec0
TyShingler <tyshingler@gmail.com>
2016-03-11 21:25:27 -0500

    Mock up method stubs for UIController

---
+++ src/model/UIController.java
@@ -0,0 +1,51 @@
+package model;
+
+public class UIController {
+
+    public void createPortfolio(){}
+
+    public void refereshHoldingAndTransactions(){}
+
+    public void importHoldingsAndTransactions(){}
+
+    public void isAuthenticated(){}
+
+    public void requestLogin(){}
+
+    public void addEquity(){}
+
+    //set the new cash account and forgets the old
+    //also to be used to add
+    public void ChangeCashAccount(){}
+
+    public void removeCashAccount(){}
+
+    public void removeEquity(){}
+
+    /*
+    Throws an exception if you try to withdraw or transfer a negative amount
+     */
+    public void makeTransaction() throws Exception{}
+
+    public void showDeposit(){}
+
+    public void showWithdrawal(){}
+
+    public void searchTickers(){}
+
+    public void displayPortfolio(){}
+
+    public void logTransaction(){}
+
+    public void viewTransactionHistory(){}
+
+    public void displaySimulationOptions(){}
+
+    public void displaySimulation(){}
+
+    public void savePortfolio(){}
+
+    public void logout(){}
+
+    public void exportPortfolioHoldingsAndTransactions(){}
+}
