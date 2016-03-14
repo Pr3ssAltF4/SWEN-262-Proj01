164f45431f07d7026a4fdba91827512cae30676c
Eric Mai <ericmai231@gmail.com>
2016-03-13 22:13:29 -0400

    Transactions class

--- src/model/Transaction.java
+++ src/model/Transaction.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class Transaction {
 
-<<<<<<< HEAD
+
     private double amount;
     private String type;
     private Equity equity;
@@ -15,15 +15,8 @@ public class Transaction {
         this.amount = amount;
         this.type = type;
         this.equity = equity;
-=======
-    public int id;
-    
-    public Transaction() {
-        this.id = IdGenorator.getInstance().getNewId();
->>>>>>> 31e79b5298a1b384a1098254cca902842eb699ab
     }
-
-
+    
 
     public String exportTransaction(){
         String export = "";
@@ -32,11 +25,10 @@ public class Transaction {
     }
 
     public static Transaction importTransaction(String line) {
-<<<<<<< HEAD
+
         String[] args = line.split(",");
         return new Transaction(args[0], Double.parseDouble(args[1]), new Equity(args[2], Integer.parseInt(args[3]), Double.parseDouble(args[4])));
-=======
-        return new Transaction();
->>>>>>> 31e79b5298a1b384a1098254cca902842eb699ab
+
+
     }
 }
