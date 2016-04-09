746c70723acf5e875db823bbf33d6b26d9b3be51
Ian <ixt7265@g.rit.edu>
2016-04-09 12:09:17 -0400

    Couple edits done to Portfolio and User

--- Portfolio.java
+++ Portfolio.java
@@ -10,10 +10,11 @@ public class Portfolio {
     private String name = "";
     private Date dateCreated = null;
     private ArrayList<Equity> equities = new ArrayList<Equity>();
+    private ArrayList<Account> accounts = new ArrayList<Account>();
     private int id = IdGenorator.getInstance().getNewId();
     private Transaction history;
     private User user;
-
+    
     public Portfolio(User user, String name) {
 	this.name = name;
 	this.user = user;
@@ -24,7 +25,10 @@ public class Portfolio {
     public Transaction getTransaction() { return this.transaction; }
     public Date getDate() { return this.date;  }
 
-    public String getUsername() {}
+    public ArrayList<Equity> getEquities() { return this.equities; }
+    public ArrayList<Account> getAccounts() { return this.accounts; }
+
+    // this might be in the wrong place...
     public boolean validatePassword(String entered_password) {
 	if (entered_password == getUser().getPassword())
 	    return true;
@@ -32,5 +36,32 @@ public class Portfolio {
 	    return false;
     }
     
+    // Method headers to complete
+
+    // (for both below) returns empty string iff the transaction was undone successfully.
+    public String undoTransaction() {
+	return ""
+    }
+    public String performTransaction() {
+	return ""
+    }
+
+    public void removeWatchedEquity() {}
+    public void addWatchedEquity() {}
+
+    public void buyEquity() {}
+    public void sellEquity() {}
+
+    public void depositInAccount() {}
+    public void withdrawFromAccount() {}
+
+    public void simulateMarket() {}
+
+    // These are done dead last.
+    public Portfolio importPortfolio() {}
+    public String exportPortfolio() {}
+
+    public void addAccount() {}
+    public void removeAccount() {}
     
 }
