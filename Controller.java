1b4a52ef8c33c0ac92ac154e046c9b3d8d605bd8
Ian <ixt7265@g.rit.edu>
2016-04-10 13:02:16 -0400

    Added Austin's preliminary controller.

---
+++ Controller.java
@@ -0,0 +1,96 @@
+package src.model;
+
+import java.util.ArrayList;
+
+public class Controller {
+
+	private LoginFrame lg;
+	
+	public Controller(){
+		lg = new LoginFrame();
+		lg.setVisible(true);
+	}
+	
+	//import
+	//don't know what I need since tyler isn't done yet
+	public void imp(){
+		
+	}
+	
+	//export
+	//don't know what I need since tyler isn't done yet
+	public void exp(){
+		
+	}
+	
+	//or save
+	public void update(){
+		
+	}
+	
+	public void addEquity(String ticker, int numberOfStocks, double pricePerStock){
+		
+	}
+	
+	public void removeEquity(){
+		
+	}
+	
+	public void updateEquity(String ticker, int numberOfStocks, double pricePerStock){
+		
+	}
+	
+	public void YahooTimer(){
+		
+	}
+	
+	public void searchEquities(String searchString){
+		
+	}
+	
+	public void newTransaction(){
+		
+	}
+	
+	public void undoTransaction(){
+		
+	}
+	
+	public void redoTransaction(){
+		
+	}
+	
+	public void addAccount(String name, double balance){
+		
+	}
+	
+	public void removeAccount(){
+		
+	}
+	
+	public void updateAccount(String name, double balance){
+		
+	}
+	
+	public void runSim(int steps, char stepSize, double pricePerStock, double percentChange){
+		
+	}
+	
+	public void resetSim(){
+		
+	}
+	
+	public void signIn(String username, String password){
+		
+	}
+	
+	public void createPortfolio(String username, String name, String password){
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
